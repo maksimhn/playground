70984465b93ff4e6f499cbc56e1ee50f041eca49
Maksim <dollydasheep@gmail.com>
2017-05-01 22:26:14 -0400

    Few more additions to the app structure: footer, header, services and shared folders. Added Bootstrap.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'This is a demo app and it showcases Pika\'s powers!';
 }
