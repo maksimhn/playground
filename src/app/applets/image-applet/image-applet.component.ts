ad756cb14c3de9fbfed0148a684c49b1e9bd8087
Maksim <dollydasheep@gmail.com>
2017-08-08 21:59:04 -0400

    Add list of uploaded and transformed images to the bottom of the page, get them via unique clientId.

--- src/app/applets/image-applet/image-applet.component.ts
+++ src/app/applets/image-applet/image-applet.component.ts
@@ -4,66 +4,86 @@ import { UploadOutput, UploadInput, UploadFile, humanizeBytes } from 'ngx-upload
 @Component({
   selector: 'app-image-applet',
   templateUrl: './image-applet.component.html',
-  styleUrls: ['./image-applet.component.css']
+  styleUrls: [ './image-applet.component.css' ]
 })
 export class ImageAppletComponent {
-    formData: FormData;
-    files: UploadFile[];
-    uploadInput: EventEmitter<UploadInput>;
-    humanizeBytes: Function;
-    dragOver: boolean;
+  downloadUrl: string;
+  clientId: string;
+  uploadUrl: string;
+  formData: FormData;
+  files: UploadFile[];
+  uploadInput: EventEmitter<UploadInput>;
+  humanizeBytes: Function;
+  dragOver: boolean;
+  results: Array<{}>;
 
-    constructor() {
-        this.files = []; // local uploading files array
-        this.uploadInput = new EventEmitter<UploadInput>(); // input events, we use this to emit data to ngx-uploader
-        this.humanizeBytes = humanizeBytes;
+
+  constructor() {
+    this.files = []; // local uploading files array
+    this.uploadInput = new EventEmitter<UploadInput>(); // input events, we use this to emit data to ngx-uploader
+    this.humanizeBytes = humanizeBytes;
+    this.results = [];
+    this.clientId = Math.random().toString(36).substring(7);
+    this.uploadUrl = 'http://localhost:8080/api/image/upload';
+    this.downloadUrl = 'http://localhost:8080/api/image/download/';
+  }
+
+  onUploadOutput(output: UploadOutput): void {
+    console.log(output); // lets output to see what's going on in the console
+    this.results.push(output);
+
+    if (output.type === 'allAddedToQueue') { // when all files added in queue
+      // uncomment this if you want to auto upload files when added
+      // const event: UploadInput = {
+      //   type: 'uploadAll',
+      //   url: '/api/image',
+      //   method: 'POST',
+      //   data: { foo: 'bar' },
+      //   concurrency: 0
+      // };
+      // this.uploadInput.emit(event);
+    } else if (output.type === 'addedToQueue') {
+      this.files.push(output.file); // add file to array when added
+    } else if (output.type === 'uploading') {
+      // update current data in files array for uploading file
+      const index = this.files.findIndex(file => file.id === output.file.id);
+      this.files[ index ] = output.file;
+    } else if (output.type === 'removed') {
+      // remove file from array when removed
+      this.files = this.files.filter((file: UploadFile) => file !== output.file);
+    } else if (output.type === 'dragOver') { // drag over event
+      this.dragOver = true;
+    } else if (output.type === 'dragOut') { // drag out event
+      this.dragOver = false;
+    } else if (output.type === 'drop') { // on drop event
+      this.dragOver = false;
+    } else if (output.type === 'done') {
+      let tempFileData = {
+        name: output.file.name,
+        index: output.file.fileIndex,
+        clienId: this.clientId,
+        fullUrl: `${this.downloadUrl}${this.clientId}/${output.file.name}`
+      };
+      this.results.push(tempFileData);
     }
+  }
 
-    onUploadOutput(output: UploadOutput): void {
-        console.log(output); // lets output to see what's going on in the console
+  startUpload(): void {  // manually start uploading
+    this.results = [];
 
-        if (output.type === 'allAddedToQueue') { // when all files added in queue
-            // uncomment this if you want to auto upload files when added
-            // const event: UploadInput = {
-            //   type: 'uploadAll',
-            //   url: '/api/image',
-            //   method: 'POST',
-            //   data: { foo: 'bar' },
-            //   concurrency: 0
-            // };
-            // this.uploadInput.emit(event);
-        } else if (output.type === 'addedToQueue') {
-            this.files.push(output.file); // add file to array when added
-        } else if (output.type === 'uploading') {
-            // update current data in files array for uploading file
-            const index = this.files.findIndex(file => file.id === output.file.id);
-            this.files[index] = output.file;
-        } else if (output.type === 'removed') {
-            // remove file from array when removed
-            this.files = this.files.filter((file: UploadFile) => file !== output.file);
-        } else if (output.type === 'dragOver') { // drag over event
-            this.dragOver = true;
-        } else if (output.type === 'dragOut') { // drag out event
-            this.dragOver = false;
-        } else if (output.type === 'drop') { // on drop event
-            this.dragOver = false;
-        }
-    }
+    const event: UploadInput = {
+      type: 'uploadAll',
+      url: this.uploadUrl,
+      method: 'POST',
+      data: {clientId: this.clientId},
+      concurrency: 5 // set sequential uploading of files with concurrency 1
+    };
 
-    startUpload(): void {  // manually start uploading
-        const event: UploadInput = {
-            type: 'uploadAll',
-            url: 'http://localhost:8080/api/image/upload',
-            method: 'POST',
-            data: { foo: 'bar' },
-            concurrency: 5 // set sequential uploading of files with concurrency 1
-        };
+    this.uploadInput.emit(event);
+  }
 
-        this.uploadInput.emit(event);
-    }
-
-    cancelUpload(id: string): void {
-        this.uploadInput.emit({ type: 'cancel', id: id });
-    }
+  cancelUpload(id: string): void {
+    this.uploadInput.emit({type: 'cancel', id: id});
+  }
 
 }
